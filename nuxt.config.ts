ca56d33f7de82409b0441d717f37ac61bf184d3f
Lautaro Pereyra <hi@imlautaro.com>
2022-06-17 19:18:20 -0300

    add pwa module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,16 @@
 import { defineNuxtConfig } from 'nuxt'
 
 export default defineNuxtConfig({
+	modules: ['@kevinmarrec/nuxt-pwa'],
+	pwa: {
+		icon: {
+			maskablePadding: 0,
+		},
+		manifest: {
+			name: 'Nuxt App',
+			short_name: 'Nuxt App',
+			theme_color: '#00DC82',
+		},
+	},
 	srcDir: 'src',
 })
